3a80ed62adc71659d5c54ec5c826517ea98f32d4
Fia-Iva <sofia-fia37@mail.ru>
2023-10-08 17:55:00 +0300

    fia

---
+++ script.js
@@ -0,0 +1,36 @@
+let a=10;
+a=alert("20");
+let b=2007;
+b=alert("2007");
+let js="Брендoн";
+alert(js);
+let d=10;
+let d2=5;
+alert(d/d2);
+let r=2**5;
+let result=32;
+alert(result);
+let a2=9;
+let b2=2;
+alert(a2/b2-3.5);
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+let age=prompt('Сколько вам лет?');
+user = {
+    name:"София",
+    age:23,
+    isAdmin:true,
+}
+user.cityofresidence ="Москва";
+user.age =25;
+delete user.cityofresidence;
+let info= prompt("Какую информацию хотите узнать о пользователе?");
+alert(user.info);
+let nam=prompt('Как вас зовут?');
+alert("Привет, София!");
