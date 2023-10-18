cfbb0603cdc4bc335d66501a659ca76dcf350d22
Fia-Iva <sofia-fia37@mail.ru>
2023-10-18 15:03:18 +0300

    fia

--- script.js
+++ script.js
@@ -96,3 +96,33 @@ switch(monthNumber){
         console.log('ввел что-то не то')  
     break;
 }
+for (let i = 10; i > 0; i--){
+    console. log (i);
+     console. log ("Привет"); 
+    }
+    for (let i = 1; i <= 5; i++){
+    console. log(i) ;
+    }
+    for (let i = 7; i <= 22; i++) { 
+    console. log(i) ;
+    }
+    let obj ={
+    Коля: "200",
+    Вася: "300",
+    Петя: "400",
+    }
+    for (let key in obj) {
+    console. log('${key} - зарплата ${obj [key]} долларов.');
+    }
+    let n = 10000;
+    let ne = 0;
+    while(n >= 50){
+        n /= 2;
+        ne ++;   
+    }
+    console. log(n) ;
+    console. log(ne) ;
+    let firstFriday = 5 ;
+    for (let i =firstFriday; i <= 31 ;i += 7 ){
+        console. log('сегодня пятница,${i}-e число.Необходимо подготовить отчет. ') ;  
+    }
