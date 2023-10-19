54cd9a3ec00fce01a4481e9060feae23a5ade56a
Fia-Iva <sofia-fia37@mail.ru>
2023-10-19 19:43:22 +0300

    fia

--- script.js
+++ script.js
@@ -37,66 +37,9 @@ user.age =25;
 delete user.cityofresidence;
 let info= prompt("Какую информацию хотите узнать о пользователе?");
 alert(user[info]);
-let nam=prompt('Как вас зовут?');
-alert('Привет, ${nam}!');
-let password ='Пароль';
-let massege= prompt('Введите пароль');
-if(massege == password){
-    massege='пароль введен верно';
-}else {
-    massege='пароль введен неверно';
-}
-alert(massege);
-let c=3;
-if(c>0 && c<10){
-    echo='верно';
-}else{
-    echo='неверно';
-}
-let d3=8;
-let e=108;
-if(a>100 || d3>100){
-    echo='верно';
-}else{
-    echo='неверно';
-}
-let aa= '2';
-let bb= '3';
-alert(Number(aa) + Number(bb));
-let monthNumber = (prompt('к какому сезону этот месяц принадлежит'));
-switch(monthNumber){
-    case'декабрь':
-    console.log('зима')
-    break;
-    case'январь':
-    console.log('зима')
-    break;
-    case'февраль':
-    console.log('зима')
-    break;
-    case'март':
-    console.log('весна')
-    break;
-    case'апрель':
-    console.log('весна')
-    break;
-    case'май':
-    console.log('весна')
-    break;
-    case'июнь':
-    console.log('лето')
-    break;
-    case'июль':
-    console.log('лето')
-    break;
-    case'август':
-    console.log('лето')
-    break;
-    default:
-        console.log('ввел что-то не то')  
-    break;
-}
-for (let i = 10; i > 0; i--){
+let nam=prompt(`Как вас зовут?`);
+alert(`Привет, ${nam}!`);
+for (let i = 2; i > 0; i--){
     console. log (i);
      console. log ("Привет"); 
     }
@@ -112,7 +55,7 @@ for (let i = 10; i > 0; i--){
     Петя: "400",
     }
     for (let key in obj) {
-    console. log('${key} - зарплата ${obj [key]} долларов.');
+    console. log(`${key} - зарплата ${obj [key]} долларов.`);
     }
     let n = 10000;
     let ne = 0;
@@ -124,5 +67,5 @@ for (let i = 10; i > 0; i--){
     console. log(ne) ;
     let firstFriday = 5 ;
     for (let i =firstFriday; i <= 31 ;i += 7 ){
-        console. log('сегодня пятница,${i}-e число.Необходимо подготовить отчет. ') ;  
+        console. log(`сегодня пятница,${i}-e число.Необходимо подготовить отчет. `) ;  
     }
