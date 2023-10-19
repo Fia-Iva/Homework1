4f5123b3c5da7de475f303689e187311e8c0e7ea
Fia-Iva <sofia-fia37@mail.ru>
2023-10-19 19:42:07 +0300

    fia

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let a=10;
 alert(a);
-ф=20;
+a=20;
 alert(a);
 const b=2007;
 alert(b);
@@ -37,5 +37,35 @@ user.age =25;
 delete user.cityofresidence;
 let info= prompt("Какую информацию хотите узнать о пользователе?");
 alert(user[info]);
-let nam=prompt('Как вас зовут?');
-alert("Привет, ${nam}!");
+let nam=prompt(`Как вас зовут?`);
+alert(`Привет, ${nam}!`);
+for (let i = 2; i > 0; i--){
+    console. log (i);
+     console. log ("Привет"); 
+    }
+    for (let i = 1; i <= 5; i++){
+    console. log(i) ;
+    }
+    for (let i = 7; i <= 22; i++) { 
+    console. log(i) ;
+    }
+    let obj ={
+    Коля: "200",
+    Вася: "300",
+    Петя: "400",
+    }
+    for (let key in obj) {
+    console. log(`${key} - зарплата ${obj [key]} долларов.`);
+    }
+    let n = 10000;
+    let ne = 0;
+    while(n >= 50){
+        n /= 2;
+        ne ++;   
+    }
+    console. log(n) ;
+    console. log(ne) ;
+    let firstFriday = 5 ;
+    for (let i =firstFriday; i <= 31 ;i += 7 ){
+        console. log(`сегодня пятница,${i}-e число.Необходимо подготовить отчет. `) ;  
+    }
